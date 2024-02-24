33db6d8f9549d615390094480676279edb84dc1b
Allan Suzuki <allan.suzuki@outlook.com>
2024-02-24 22:44:34 +0000

    add project readme link on github

--- DS-PY-Data-Science/DS-PY-017 LÓGICA DE PROGRAMAÇÃO II (PY)/Material do Aluno/projeto_transacoes_bancarias.py
+++ DS-PY-Data-Science/DS-PY-017 LÓGICA DE PROGRAMAÇÃO II (PY)/Material do Aluno/projeto_transacoes_bancarias.py
@@ -1,4 +1,6 @@
 # projeto_transacoes_bancarias -- DS-PY-17 - logica de programacao II
+# readme link here: 
+# https://github.com/allansuzuki/ADA_classes/blob/main/DS-PY-Data-Science/DS-PY-017%20L%C3%93GICA%20DE%20PROGRAMA%C3%87%C3%83O%20II%20(PY)/Material%20do%20Aluno/projeto_README.md
 # 
 # Esse programa é um sistema de gestao de transacoes de uma conta bancária pessoal
 # no qual os dados são de transações e possuem seu valor, a categoria do gasto e seu ID.
